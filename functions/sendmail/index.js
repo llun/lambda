00996fad8fa14e5b21bbdd26502bd556182eb9f4
Maythee Anegboonlap <contact@llun.dev>
2023-04-19 21:29:01 +0200

    Add event type

--- functions/sendmail/index.js
+++ functions/sendmail/index.js
@@ -1,27 +1,41 @@
+// @ts-check
 const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
 
+/**
+ * @typedef {{ from: string, to: string[], replyTo?: string, subject: string, content: { text: string, html: string }, configurationSet?: 'transaction-emails' }} Event
+ */
+
+/**
+ *
+ * @param {Event} event
+ * @param {import('aws-lambda').Context} context
+ *
+ * @returns
+ */
 exports.entry = async function (event, context) {
   const client = new SESClient({ region: "eu-central-1" });
   const input = {
-    Source: "accounts@llun.social",
+    Source: event.from,
     Destination: {
-      ToAddresses: ["social-test@llun.dev"],
+      ToAddresses: event.to,
     },
     Message: {
       Subject: {
-        Data: "Account verification email",
+        Data: event.subject,
       },
       Body: {
         Text: {
-          Data: "This is a test email",
+          Data: event.content.text,
         },
         Html: {
-          Data: "<strong>This</strong> is a test email",
+          Data: event.content.html,
         },
       },
     },
-    ReplyToAddresses: ["accounts@llun.social"],
-    ConfigurationSetName: "transaction-emails",
+    ...(event.replyTo && { ReplyToAddresses: [event.replyTo] }),
+    ...(event.configurationSet && {
+      ConfigurationSetName: event.configurationSet,
+    }),
   };
   const command = new SendEmailCommand(input);
   const response = await client.send(command);
