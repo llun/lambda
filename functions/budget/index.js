daa287543bd015ae5dc4433e57c69520adf41259
Maythee Anegboonlap <contact@llun.dev>
2024-03-02 08:42:25 +0100

    Prepare budget function

--- functions/budget/index.js
+++ functions/budget/index.js
@@ -1,4 +1,13 @@
 exports.entry = async function (event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-  return context.logStreamName;
+  if (!("Records" in event)) return false;
+
+  const records = event.Records.filter(
+    (record) => record.EventSource === "aws:sns"
+  );
+  const exceeded = event.Records.find((record) => {
+    return record.Sns.Subject.includes("has exceeded your alert threshold");
+  });
+  if (!exceeded) return false;
+
+  return true;
 };
